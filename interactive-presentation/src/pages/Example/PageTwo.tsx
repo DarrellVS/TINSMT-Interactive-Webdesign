8bd4d7bd39be2eb81da5a5f4aa9d565c6d75d009
Darrell van Swinderen <darrell260802@gmail.com>
2022-11-23 13:55:46 +0100

    Final Push

--- interactive-presentation/src/pages/Example/PageTwo.tsx
+++ interactive-presentation/src/pages/Example/PageTwo.tsx
@@ -1,14 +1,36 @@
+import { Box, Text, Button } from "@chakra-ui/react";
+import { useState } from "react";
 import FootNote from "../../components/FootNote";
 import Layout from "../../components/Layout";
 import PageHeading from "../../components/PageHeading";
 import { AnimatedPageTransition } from "../../components/SlideWrapper";
+
+function Counter() {
+  const stateArray = useState(0);
+  const [waarde, zetWaarde] = stateArray;
+
+  const increment = () => zetWaarde((w) => w + 1);
+  const decrement = () => zetWaarde((w) => w - 1);
+
+  const kleur = waarde % 2 === 0 ? "red" : "gray";
+
+  return (
+    <Box>
+      <Button onClick={increment}>+</Button>
+      <Button onClick={decrement}>-</Button>
+      <Text color={kleur}>{waarde}</Text>
+    </Box>
+  );
+}
 
 export default function ExamplePageTwo() {
   return (
     <Layout>
       <PageHeading query="Samen">Nu samen.</PageHeading>
 
-      <AnimatedPageTransition>{/* Hier de counter! */}</AnimatedPageTransition>
+      <AnimatedPageTransition>
+        <Counter />
+      </AnimatedPageTransition>
 
       <FootNote />
     </Layout>
